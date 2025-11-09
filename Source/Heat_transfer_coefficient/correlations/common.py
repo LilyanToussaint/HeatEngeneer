27270533207c15f738598092770492a4e342025b
LilyanToussaint <lilyantoussaint@gmail.com>
2025-11-09 01:59:41 +0100

    Remove fin efficiency helpers and add common import

---
+++ Source/Heat_transfer_coefficient/correlations/common.py
@@ -0,0 +1,8 @@
+"""Shared helpers for heat-transfer coefficient correlations."""
+from __future__ import annotations
+
+from ...common import reynolds_number as _reynolds_number
+
+reynolds_number = _reynolds_number
+
+__all__: list[str] = []
